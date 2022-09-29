ca58efdd7f30c13859a7c185029798de51f9ab8b
unknown <machodin@yandex.ru>
2022-09-29 15:41:49 +0300

    feat: added header

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1 className='header__heading'>Goga shop</h1>
+        <div className='header__rightside-menu'>
+           <button className='header__rightside-menu_button'>Какая то кнопка</button>
+           <button className='header__rightside-menu_button'>Какая то кнопка</button>
+           <button className='header__rightside-menu_button'>Какая то кнопка</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
